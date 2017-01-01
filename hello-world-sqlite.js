a660943dc3e16ba5d6c9c9098eb0c7574de4c3c8
Nick Sondej <nick@markforged.com>
2016-12-31 19:37:35 -0500

    More work in progress

--- hello-world-sqlite.js
+++ hello-world-sqlite.js
@@ -1,10 +1,23 @@
-var db = require('sqlite');
 var fs = require('fs');
+var fn = 'hello-world.db';
+var dbFile = './' + fn
+var exists = fs.existsSync(dbFile);
 
-Promise.resolve()
-    // Open db
-    .then(() => db.open('./hello-world.db', { Promise }))
-    .catch(err => console.error(err.stack));
 
-    // 
+// Check if DB file exists, create if not
+if(!exists){
+    console.log("DB file does not exist")
+    fs.openSync(dbFile, 'w');
+    console.log("DB file created: " + fn)
+}
 
+// Open DB file
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(dbFile);
+
+db.serialize(function() {
+    if(!exists) {
+        db.run("CREATE TABLE Stuff (thing TEXT)");
+    }
+    db.run(".schema");
+})
